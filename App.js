7e87ad4ea70d9d54f40b40a35294acd98b7e525f
anas-hatim <hatim.anas93@gmail.com>
2023-03-01 09:38:52 +0100

    v1.0.0

--- App.js
+++ App.js
@@ -1,128 +1,145 @@
-import React, {useState} from 'react';
-import { Platform, StyleSheet, Text, View , TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { Platform, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView, ImageBackground, Modal } from 'react-native';
 import { KeyboardAvoidingView } from 'react-native';
 import Task from './components/Task';
+import { SwipeListView } from 'react-native-swipe-list-view';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
   const [task, setTask] = useState();
   const [taskItems, setTaskItems] = useState([]);
-  
+  const image = require('./assets/Gradient-Phone-Wallpaper-148.jpg');
+
+
   const handleAddTask = () => {
     Keyboard.dismiss();
     setTaskItems([...taskItems, task])
     setTask(null);
-    
+
   }
 
-const completeTask = (index) => {
-  let itemsCopy = [...taskItems];
-  itemsCopy.splice (index, 1);
-  setTaskItems(itemsCopy);
-}
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
+  const editTask = (index, text) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy[index] = text;
+    setTaskItems(itemsCopy);
+  }
+
+  const onSwipeRight = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
+  const onSwipeLeft = (index) => {
+    // Handle task edit
+    console.log('Edit task at index', index);
+  }
+
+  const renderItem = ({ item, index }) => {
+    return (
+      <TouchableOpacity onPress={() => completeTask(index)}>
+        <Task text={item} />
+      </TouchableOpacity>
+    );
+  };
+
+  const renderHiddenItem = ({ item, index }) => {
+    return (
+      <View style={styles.rowBack}>
+        <TouchableOpacity
+          style={[styles.backRightBtn, styles.backRightBtnRight]}
+          onPress={() => onSwipeRight(index)}
+        >
+          <AntDesign name="delete" size={24} color="#FFF" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.backRightBtn, styles.backRightBtnLeft]}
+          onPress={() => onSwipeLeft(index)}
+        >
+          <AntDesign name="edit" size={24} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
         <ScrollView
-        contentContainerStyle={{
-          flexGrow: 1
-        }}
-        keyboardShouldPersistTaps='handled'
-      >
-      {/* { Today's task} */}
-      <View style={styles.taskWarpper}>
-        <Text style={styles.sectionTitle}>TO DO LIST BABY </Text>
+          contentContainerStyle={{
+            flexGrow: 1
+          }}
+          keyboardShouldPersistTaps='handled'
+        >
+          {/* { Today's task} */}
+          <View style={styles.taskWarpper}>
+            <Text style={styles.sectionTitle}>TO DO LIST BABY </Text>
 
-        <View style={styles.items}>
-          {/* {this is where the taskq will go} */}
-          {
-            taskItems.map((item, index) => {
-              return (
-              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
-                <Task text={item} />
-              </TouchableOpacity>
-              )
-            })
-          }
-          {/* <Task text='mangfghjker'/>
-          <Task text='dormir'/>
-          <Task text='courir'/>
-          <Task text='rien faire'/> */}
-        
-        </View>
+            <SwipeListView
+              data={taskItems}
+              renderItem={renderItem}
+              renderHiddenItem={renderHiddenItem}
+              rightOpenValue={-75}
+              leftOpenValue={75}
+              disableRightSwipe={true}
+            />
 
-      </View>
-      </ScrollView>
-      
-      {/* {write a task} */}
-      <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={styles.whriteTaskWrapper}
-      >
-        <TextInput style={styles.input} placeholder={'Write a Task'} value={task} onChangeText={text => setTask(text)} />
-        <TouchableOpacity onPress={() => handleAddTask()}>
-          <View style={styles.addWrapper}>
-            <Text style={styles.addText}>+</Text>
           </View>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
+        </ScrollView>
+
+        {/* {write a task} */}
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={styles.writeTaskWrapper}
+        >
+          <TextInput style={styles.input} placeholder={'Write a Task'} value={task} onChangeText={text => setTask(text)} />
+          <TouchableOpacity onPress={() => handleAddTask()}>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </ImageBackground>
     </View>
-  );
+    );
 }
-
-
-
-const styles = StyleSheet.create({
-
-  container: {
-    flex: 1,
-    backgroundColor: '#E8EAED',
-  },
-
-taskWarpper:{
-  paddingTop: 80,
-  paddingHorizontal: 20,
-},
-
-sectionTitle:{
-  paddingBottom: 30,
-  fontsize: 25,
-  fontWeight: 'bold',
-  color:'#20B2AA',
-  fontSize: 24,
-},
-
-items:{
-marginTop: 30,
-},
-whriteTaskWrapper:{
-  position:'absolute',
-  bottom:60,
-  width:'100%',
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  alignItems:'center',
-
-},
-input: {
-  marginLeft:25,
-  paddingVertical: 15,
-  paddingHorizontal: 15,
-  backgroundColor: '#FFF',
-  borderRadius: 60,
-  borderColor: '#C0C0C0',
-  borderWidth: 1,
-  width: 250,
-},
-addWrapper: {
-  marginRight:25,
-  width: 60,
-  height: 60,
-  backgroundColor: '#FFF',
-  borderRadius: 60,
-  justifyContent: 'center',
-  alignItems: 'center',
-  borderColor: '#C0C0C0',
-  borderWidth: 1,
-  
-},
-addText: {},
-});
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+      },
+      image: {
+        flex: 1,
+        justifyContent: 'center',
+      },
+      taskWarpper: {
+        paddingTop: 80,
+        paddingHorizontal: 20,
+      },
+      sectionTitle: {
+        paddingBottom: 30,
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 24,
+        textAlign: 'center',
+      },
+      items: {
+        marginTop: 30,
+      },
+      writeTaskWrapper: {
+        position: 'absolute',
+        bottom: 60,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+      },
+      input: {
+        paddingVertical: 15
+      },
+    });
--- components/task.js
+++ components/task.js
@@ -1,56 +1,79 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity} from "react-native";
+import { AntDesign } from '@expo/vector-icons';
+import Swipeable from 'react-native-swipeable';
 
+const Task = (props) => {
+  const rightButtons = [
+    <TouchableOpacity
+      onPress={() => props.onDelete()}
+      style={[styles.button, styles.rightButton]}
+    >
+      <AntDesign name="delete" size={24} color="#FFFFFF" />
+    </TouchableOpacity>,
+  ];
 
-const Task = (props) =>{
+  const leftButtons = [
+    <TouchableOpacity
+      onPress={() => props.onEdit()}
+      style={[styles.button, styles.leftButton]}
+    >
+      <AntDesign name="edit" size={24} color="#FFFFFF" />
+    </TouchableOpacity>,
+  ];
 
-    return(
-        <View style={styles.item}>
-            <View style={styles.itemLeft}>
-                <View style={styles.square}></View>
-                <Text style={styles.itemText}>{props.text}</Text>  
-                
-            </View>
-            <View style={styles.circular}></View>
-    
+  return (
+    <Swipeable
+      rightButtons={rightButtons}
+      leftButtons={leftButtons}
+    >
+      <View style={styles.item}>
+        <View style={styles.itemLeft}>
+          <View style={styles.square}></View>
+          <Text style={styles.itemText}>{props.text}</Text>
         </View>
-    )
-}
+      </View>
+    </Swipeable>
+  );
+};
 
 const styles = StyleSheet.create({
-    item: {
-        backgroundColor: '#FFF',
-        padding: 15,
-        borderRadius: 10,
-        flexDirection:'row',
-        alignItems:'center',
-        justifyContent: 'space-between',
-        marginBottom: 20,
-    },
-    itemLeft: {
-        flexDirection: 'row',
-        alignItems: 'center', 
-        flexWrap:'wrap',
-    },
-    square: {
-        width: 20,
-        height: 24,
-        backgroundColor:'#55BCF6',
-        opacity: 0.4,
-        borderRadius: 5,
-        marginRight: 15,
-    },
-    itemText: {
-        maxWidth:'80%',
-    },
-    circular: {
-        width: 12,
-        height: 12,
-        backgroundColor:'#F00',
-        borderwidth: 2,
-        borderRadius: 5,
-        marginLeft: 40
-    },
+  item: {
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  square: {
+    width: 20,
+    height: 24,
+    backgroundColor: '#55BCF6',
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: '80%',
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rightButton: {
+    backgroundColor: 'red',
+  },
+  leftButton: {
+    backgroundColor: 'green',
+  },
 });
-export default Task;
 
+export default Task;
